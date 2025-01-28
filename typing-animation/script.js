f0dcacc85d7559bc66f6cf117ee117a97a0cbe99
jh328 <wnsgks32810@naver.com>
2025-01-28 17:57:24 +0900

    update

--- typing-animation/script.js
+++ typing-animation/script.js
@@ -2,7 +2,7 @@ const h2 = document.querySelector(".blinking__txt");
 const cursor = document.querySelector(".cursor");
 
 const typing = function (_, counter = 0) {
-    const txt = "벌써 퇴사 하고 싶어요. 이 길이 제 길이 맞을까요..."
+    const txt = "안녕하세요. 송준한입니담."
 
     setInterval(() => {
 
